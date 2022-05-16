10aeb706de46334db8216c65b2353c8d8683efc1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-05-16 17:47:42 +0300

    Cleanup examples

--- examples/apollo/vite.config.ts
+++ examples/apollo/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    process: JSON.stringify({
+      env: {
+        DEBUG: 1,
+      },
+    }),
+  },
 })
