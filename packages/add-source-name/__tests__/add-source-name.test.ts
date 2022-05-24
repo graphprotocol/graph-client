10816ca29094a3f8f7264546c15c1e4fd494b60f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-05-24 13:05:54 +0300

    New Transform: Add Source Name

---
+++ packages/add-source-name/__tests__/add-source-name.test.ts
@@ -0,0 +1,48 @@
+import { MeshTransformOptions } from '@graphql-mesh/types'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { wrapSchema } from '@graphql-tools/wrap'
+import { execute, parse } from 'graphql'
+import AddSourceNameTransform from '../src'
+
+describe('Add Source Name Transform', () => {
+  it('should resolve the source name correctly', () => {
+    const schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          foo: Foo
+        }
+        type Foo {
+          id: ID
+        }
+      `,
+      resolvers: {
+        Query: {
+          foo: () => ({ id: 'bar' }),
+        },
+      },
+    })
+    const wrappedSchema = wrapSchema({
+      schema,
+      transforms: [new AddSourceNameTransform({ apiName: 'test' } as MeshTransformOptions)],
+    })
+    const result = execute({
+      schema: wrappedSchema,
+      document: parse(/* GraphQL */ `
+        query {
+          foo {
+            id
+            sourceName
+          }
+        }
+      `),
+    })
+    expect(result).toEqual({
+      data: {
+        foo: {
+          id: 'bar',
+          sourceName: 'test',
+        },
+      },
+    })
+  })
+})
---
+++ packages/add-source-name/src/index.ts
@@ -0,0 +1,23 @@
+import { MeshTransform, MeshTransformOptions } from '@graphql-mesh/types'
+import { MapperKind, mapSchema } from '@graphql-tools/utils'
+import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+
+export default class AddSourceNameTransform implements MeshTransform {
+  noWrap = true
+  apiName: string
+  constructor({ apiName }: MeshTransformOptions) {
+    this.apiName = apiName
+  }
+  transformSchema(schema: GraphQLSchema) {
+    return mapSchema(schema, {
+      [MapperKind.OBJECT_TYPE]: (type) => {
+        const typeConfig = type.toConfig()
+        typeConfig.fields.sourceName = {
+          type: GraphQLString,
+          resolve: () => this.apiName,
+        }
+        return new GraphQLObjectType(typeConfig)
+      },
+    })
+  }
+}
