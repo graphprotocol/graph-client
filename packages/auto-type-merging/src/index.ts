76c4c39ab2624b61b8e92b186146bdbe0b0f62b1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-05 17:46:33 +0300

    Use type merging transform for the reference

--- packages/auto-type-merging/src/index.ts
+++ packages/auto-type-merging/src/index.ts
@@ -1,26 +1,32 @@
-import { MeshTransform } from '@graphql-mesh/types'
+import { MeshTransform, MeshTransformOptions, YamlConfig } from '@graphql-mesh/types'
 import { SubschemaConfig } from '@graphql-tools/delegate'
-import { getNamedType, GraphQLSchema } from 'graphql'
+import { GraphQLSchema } from 'graphql'
+import TypeMergingTransform from '@graphql-mesh/transform-type-merging'
 
 export default class AutoTypeMergingTransform implements MeshTransform {
+  constructor(private options: MeshTransformOptions) {}
   transformSchema(schema: GraphQLSchema, subschemaConfig: SubschemaConfig) {
     const queryType = schema.getQueryType()
     const queryFields = queryType?.getFields()
+    const typeMergingQueryFieldConfig: YamlConfig.MergedRootFieldConfig[] = []
     if (queryFields != null) {
       for (const queryFieldName in queryFields) {
         const queryField = queryFields[queryFieldName]
         if (queryField.args.some((arg) => arg.name === 'where')) {
-          subschemaConfig.merge = subschemaConfig.merge || {}
-          const namedType = getNamedType(queryField.type)
-          subschemaConfig.merge[namedType.name] = {
-            fieldName: queryFieldName,
-            selectionSet: '{ id }',
-            key: ({ id }) => id,
-            argsFromKeys: (id_in) => ({ where: { id_in } }),
-          }
+          typeMergingQueryFieldConfig.push({
+            queryFieldName,
+            keyField: 'id',
+            keyArg: 'where.id_in',
+          })
         }
       }
     }
-    return schema
+    const typeMergingTransform = new TypeMergingTransform({
+      ...this.options,
+      config: {
+        queryFields: typeMergingQueryFieldConfig,
+      },
+    })
+    return typeMergingTransform.transformSchema(schema, subschemaConfig)
   }
 }
