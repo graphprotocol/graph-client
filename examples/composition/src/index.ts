469f45272eb9be77691ee3fd138c94afe4a8cce2
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-03-14 18:09:44 +0300

    Go

--- examples/composition/src/index.ts
+++ examples/composition/src/index.ts
@@ -1,10 +1,10 @@
-import { getExecutorForFusiongraph } from "@graphprotocol/client-runtime";
-import fusiongraph from '../fusiongraph';
-import { parse } from "graphql";
+import { getExecutorForFusiongraph } from '@graphprotocol/client-runtime'
+import fusiongraph from '../fusiongraph'
+import { parse } from 'graphql'
 
 const executor = getExecutorForFusiongraph({
   fusiongraph,
-});
+})
 
 async function main() {
   const response = await executor({
@@ -13,30 +13,27 @@ async function main() {
         # Shared unique identifier field
         id
         # Field from uniswap source
-        tokens(first: 1) {
-        id
-        uri
-          registry {
+        ERC721tokens(first: 1) {
+          id
+          uri
+        }
+        # Field from nft source
+        liquidityPositions(first: 1) {
           id
         }
       }
-        # Field from nft source
-        liquidityPositions(first: 1) {
-        id
-      }
-    }
 
       query ExampleQuery {
         # Get base entity from nft source
         account(id: "0x72ba1965320ab5352fd6d68235cc3c5306a6ffa2") {
           ...UserFields
-    }
+        }
         # Get base entity from uniswap source
         user(id: "0x72ba1965320ab5352fd6d68235cc3c5306a6ffa2") {
-      ...UserFields
-    }
+          ...UserFields
+        }
       }
-    `)
+    `),
   })
 
   console.log(response)
--- packages/serve-cli/src/index.ts
+++ packages/serve-cli/src/index.ts
@@ -1,17 +1,15 @@
-import { runServeCLI } from '@graphql-mesh/serve-cli';
+import { runServeCLI } from '@graphql-mesh/serve-cli'
 
 function runGraphClientServeCLI() {
-    return runServeCLI({
-        defaultConfigFileName: 'graphclient.config.ts',
-        defaultConfig: {
-            fusiongraph: 'fusiongraph.ts',
-        },
-        productName: 'GraphClient',
-    })
+  return runServeCLI({
+    defaultConfigFileName: 'graphclient.config.ts',
+    defaultConfig: {
+      fusiongraph: './fusiongraph.ts',
+    },
+    productName: 'GraphClient',
+  })
 }
 
-export {
-    defineConfig,
-} from '@graphql-mesh/serve-cli';
+export { defineConfig } from '@graphql-mesh/serve-cli'
 
-export { runGraphClientServeCLI as runServeCLI };
+export { runGraphClientServeCLI as runServeCLI }
